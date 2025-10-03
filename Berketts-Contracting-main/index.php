0a4fa434190db10e361ef9ae8ddc58d646e34bd7
Hannah ross <hannah.ross51@gmail.com>
2025-10-03 15:27:39 +1300

    fixed merg issues

--- Berketts-Contracting-main/index.php
+++ Berketts-Contracting-main/index.php
@@ -3,22 +3,6 @@
 
 <head>
     <meta charset="UTF-8">
-<<<<<<< HEAD
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Berkett Contracting LTD</title>
-
-    <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="./img/berkett-imgs/digger.png">
-
-    <!-- CSS here -->
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-=======
     <meta name="description" content="Berkett Contracting LTD specializes in comprehensive construction services including infrastructure development, forestry roading, bulk aggregate supply, and earthworks solutions. Contact us today for reliable and professional contracting needs in New Zealand.">
     <meta name="keywords" content="construction services, infrastructure development, forestry roading, bulk aggregate supply, earthworks solutions, New Zealand contracting, reliable construction, professional contracting, civil construction, quarrying, earthmoving, mobile crushing, screening equipment">
     <meta name="author" content="Hannah Ross, Web Developer">
@@ -49,7 +33,6 @@
 
 <body style="visibility: hidden;">
 
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
     <!-- Preloader -->
     <div id="preloader">
         <div class="loader"></div>
@@ -65,32 +48,17 @@
         <div class="slider-active owl-carousel ">
             <!-- Single Slider Area -->
             <div class="single-slider">
-<<<<<<< HEAD
-                <div class="slider-img position-absolute" ><img src="https://i.imgur.com/9mw7eBi.jpeg"  alt="tennis court">
-
-            </div>
-=======
                 <div class="slider-img position-absolute " loading="lazy" width="1900" height="503"><img  class="lozad" src="./img/berkett-imgs/corasal/Tennis Court - Wells.webp" alt="tennis court"
                 style="">
                 </div>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                 <div class="container ">
                     <div class="row">
                         <div class="col-lg-9">
                             <div class="slider-content ">
-<<<<<<< HEAD
-                                <p class="text-light" data-animation="fadeInDown" data-delay="900ms">29 Years
-                                    providing local expertise</p>
-                                <h3 class="text-light" data-animation="fadeInDown" data-delay="400ms">Expert Infrastructure Construction</h3>
-                                <div class="text-light" class="slider-btn mt-30" data-animation="fadeInDown"
-                                    data-delay="90ms">
-                                    <a class="boxed-btn2" href="#">Infrastructure Services <i
-=======
                                 <h3 class="text-light" data-animation="fadeInDown" data-delay="400ms">Complex Infrastructure Development</h3>
                                 <div class="text-light" class="slider-btn mt-30" data-animation="fadeInDown"
                                     data-delay="90ms">
                                     <a class="boxed-btn2" href="./service-details.php?service=infrastructure">Infrastructure Services <i
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                                             class="Flaticon flaticon-right-arrow"></i></a>
                                 </div>
                             </div>
@@ -101,29 +69,16 @@
 
             <!-- Single Slider Area -->
             <div class="single-slider">
-<<<<<<< HEAD
-                <div class="slider-img position-absolute"><img src="./img/berkett-imgs/corasal/digger.jpg"></div>
-=======
                 <div class="slider-img position-absolute" loading="lazy" width="1900" height="503"><img class="lozad" src="./img/berkett-imgs/corasal/digger.webp" alt="blue digger"
                 style=""></div>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                 <div class="container ">
                     <div class="row">
                         <div class="col-lg-9">
                             <div class="slider-content ">
-<<<<<<< HEAD
-                                <p class="text-light" data-animation="fadeInDown" data-delay="900ms">29 Years
-                                    providing local expertise</p>
-                                <h3 class="text-light" data-animation="fadeInDown" data-delay="400ms"> Forestry Roading & Bulk Aggregate Supply</h3>
-                                <div class="text-light" class="slider-btn mt-30" data-animation="fadeInDown"
-                                    data-delay="90ms">
-                                    <a class="boxed-btn2" href="#">Forestry Services <i
-=======
                                 <h3 class="text-light" data-animation="fadeInDown" data-delay="400ms"> Forestry Roading & Bulk Aggregate Supply</h3>
                                 <div class="text-light" class="slider-btn mt-30" data-animation="fadeInDown"
                                     data-delay="90ms">
                                     <a class="boxed-btn2" href="./service-details.php?service=forestry">Forestry Services <i
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                                             class="Flaticon flaticon-right-arrow"></i></a>
                                 </div>
                             </div>
@@ -134,28 +89,15 @@
 
             <!-- Single Slider Area -->
             <div class="single-slider">
-<<<<<<< HEAD
-                <div class="slider-img position-absolute"><img src="./img/berkett-imgs/corasal/trucks.jpg"></div>
-=======
                 <div class="slider-img position-absolute" loading="lazy" width="1900" height="503"><img class="lozad" src="./img/berkett-imgs/corasal/trucks.webp" alt="two trucks" style=""></div>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                 <div class="container ">
                     <div class="row">
                         <div class="col-lg-9">
                             <div class="slider-content ">
-<<<<<<< HEAD
-                                <p class="text-light" data-animation="fadeInDown" data-delay="900ms">29 Years
-                                    providing local expertise</p>
-                                <h3 class="text-light" data-animation="fadeInDown" data-delay="400ms">Comprehensive Land Development</h3>
-                                <div class="text-light" class="slider-btn mt-30" data-animation="fadeInDown"
-                                    data-delay="90ms">
-                                    <a class="boxed-btn2" href="#">Earthwork Services <i
-=======
                                 <h3 class="text-light" data-animation="fadeInDown" data-delay="400ms">Earthworks solutions</h3>
                                 <div class="text-light" class="slider-btn mt-30" data-animation="fadeInDown"
                                     data-delay="90ms">
                                     <a class="boxed-btn2" href="./service-details.php?service=earthworks">Earthwork Services <i
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                                             class="Flaticon flaticon-right-arrow"></i></a>
                                 </div>
                             </div>
@@ -176,18 +118,6 @@
                 <div class="col-xl-6 col-md-6">
                     <div class="single-service ">
 
-<<<<<<< HEAD
-                        <div class="service-thumb">
-                            <img src="./img/berkett-imgs/services/quarry-truck.jpg" alt="">
-                        </div>
-                        <div class="">
-                            <h3>Gravel & Quarry</h3>
-                            <p> With two local quarries and mobile crushing and screening equipment, Berkett Contracting
-                                can provide bulk aggregate supply for commercial construction, farming and forestry
-                                operations. </p>
-                        </div>
-                        <a href="./service-details.php html" class="read-more mt-auto">Read More</a>
-=======
                         <div class="service-thumb" loading="lazy" width="540" height="406">
                             <img class="img-fluid" loading="lazy" src="./img/berkett-imgs/services/quarry-truck.webp" alt="quarry truck" style="max-width: 540px; max-max-height: 405px;">
                         </div>
@@ -196,7 +126,6 @@
                             <p> With two local quarries and mobile crushing and screening equipment, Berkett Contracting can provide bulk aggregate supply for commercial and industrial construction, farming, and forestry operations.</p>
                         </div>
                         <a href="./service-details.php?service=gravel" class="read-more mt-auto">Read More</a>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                     </div>
                 </div>
 
@@ -204,18 +133,6 @@
                 <div class="col-xl-6 col-md-6">
                     <div class="single-service">
 
-<<<<<<< HEAD
-                        <div class="service-thumb">
-                            <img src="./img/berkett-imgs/services/construction-digger.jpg" alt="">
-                        </div>
-                        <div class="">
-                            <h3>Infrastructure</h3>
-                            <p> We provide services to construct a wide range of commercial & industrial infrastructure
-                                including drainage and pipework, roading and carpark construction, retaining walls,
-                                walkways and cycleways.</p>
-                        </div>
-                        <a href="./service-details.php html" class="read-more mt-auto">Read More</a>
-=======
                         <div class="service-thumb" loading="lazy" width="540" height="406">
                             <img loading="lazy" src="./img/berkett-imgs/services/construction-digger.webp" alt="construction digger" style="max-width: 540px; max-max-height: 405px;">
                         </div>
@@ -224,7 +141,6 @@
                             <p> We provide services to construct a wide range of commercial & industrial infrastructure including drainage and pipework, roading and car park construction, retaining walls, walkways, and cycleways.</p>
                         </div>
                         <a href="./service-details.php?service=infrastructure" class="read-more mt-auto">Read More</a>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                     </div>
                 </div>
 
@@ -236,18 +152,6 @@
                 <div class="col-xl-6 col-md-6">
                     <div class="single-service">
 
-<<<<<<< HEAD
-                        <div class="service-thumb ">
-                            <img src="./img/berkett-imgs/services/digger-on-clay.jpg" alt="digger">
-                        </div>
-                        <div class="">
-                            <h3>Earthworks</h3>
-                            <p>Commercial, industrial, and rural projects ranging from roading, shed or house sites to
-                                large scale building platforms, and detailed footings and foundations of large
-                                commercial developments.</p>
-                        </div>
-                        <a href="./service-details.php html" class="read-more mt-auto">Read More</a>
-=======
                         <div class="service-thumb " loading="lazy" width="540" height="406">
                             <img loading="lazy" src="./img/berkett-imgs/services/digger-on-clay.webp" alt="digger on clay" style="max-width: 540px; max-height: 405px;">
                         </div>
@@ -256,26 +160,11 @@
                             <p>Commercial, industrial, and rural projects ranging from roading, shed or house sites to large scale building platforms, and detailed footings and foundations of large commercial developments.</p>
                         </div>
                         <a href="./service-details.php?service=earthworks" class="read-more mt-auto">Read More</a>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                     </div>
                 </div>
 
                 <!-- Single Service Area -->
                 <div class="col-xl-6 col-md-6">
-<<<<<<< HEAD
-                    <div class="single-service">
-
-                        <div class="service-thumb">
-                            <img src="./img/berkett-imgs/services/forestry-digger.jpg" alt="">
-                        </div>
-                        <div class="">
-                            <h3>Forestry</h3>
-                            <p> Our experienced forestry team have the skills to provide services for resource
-                                consenting, design, construction, maintenance and post-harvest clean-up for forestry
-                                infrastructure.</p>
-                        </div>
-                        <a href="./service-details.php html" class="read-more mt-auto">Read More</a>
-=======
                     <div class="single-service" >
 
                         <div class="service-thumb" loading="lazy" width="540" height="406">
@@ -287,7 +176,6 @@
                             </p>
                         </div>
                         <a href="./service-details.php?service=forestry" class="read-more mt-auto">Read More</a>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                     </div>
                 </div>
 
@@ -309,17 +197,6 @@
             <div class="row align-items-center">
                 <div class="col-lg-6">
                     <div class="section-title">
-<<<<<<< HEAD
-                        <span>How Berketts Dose it</span>
-                        <h3>By Covering All Your Contracting Needs</h3>
-                        <p class="para-text">
-                            Established in 1995, Berkett Contracting is a family owned and operated earthmoving business
-                            that specilises in
-                            small to medium earthworks, civil construction and quarrying.
-
-                        </p>
-                        <a href="./about.html" class="boxed-btn">Learn More</a>
-=======
                         <span>How Berkett's Dose it</span>
                         <h3>By Covering All Your Contracting Needs</h3>
                         <p class="para-text">
@@ -328,19 +205,13 @@
 
                         </p>
                         <a href="./about.php" class="boxed-btn">Learn More</a>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                     </div>
                 </div>
 
                 <!-- About Thumb -->
                 <div class="col-lg-6">
-<<<<<<< HEAD
-                    <div class="about-thumb">
-                        <img src="./img/berkett-imgs/sunset digger.jpg" alt="">
-=======
                     <div class="about-thumb" loading="lazy" width="540" height="181">
                         <img loading="lazy" src="./img/berkett-imgs/sunset digger.webp" alt="sunset digger" style="max-width: 600px; max-height: 200px;">
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                     </div>
                 </div>
             </div>
@@ -350,32 +221,18 @@
     <!-- About end -->
 
     <!-- !cta-area-start -->
-<<<<<<< HEAD
-    <div class="cta-area bg-img bg-overlay-2"
-        style="background-image: url(./img/berkett-imgs/landscape-lake-pukaki-pukaki-new-zealand-surrounded-with-snowy-mountains.jpg)">
-=======
     <div class="cta-area bg-img bg-overlay-2" loading="lazy" width="1408" height="672"
         style="background-image: url(./img/berkett-imgs/landscape-lake-pukaki-pukaki-new-zealand-surrounded-with-snowy-mountains.webp)">
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
         <div class="container">
             <div class="col-xl-6 col-lg-7">
                 <!-- Call to action Content -->
                 <div class="cta-content">
                     <!-- <h4 class="text-light font-weight-light">Lets have a chat</h4> -->
                     <h3>Have A Project In Mind?</h3>
-<<<<<<< HEAD
-                    <p>Waters make fish every without firmament saw had. <br> Morning air subdue very one. Whales grass
-                        is
-                        fish <br> whales winged.</p>
-                    <!-- Call to action Buton -->
-                    <div class="cta-btn">
-                        <a class="boxed-btn2 black-bg" href="#">Discuss now <i
-=======
                     <p>Ready to start your next project? Contact us to discuss your ideas</p>
                     <!-- Call to action Buton -->
                     <div class="cta-btn">
                         <a class="boxed-btn2 black-bg" href="./contact.php">Lets Talk <i
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
                                 class="Flaticon flaticon-right-arrow"></i></a>
                     </div>
                 </div>
@@ -384,7 +241,6 @@
     </div>
     <!-- cta-area-end -->
 
-<<<<<<< HEAD
      <!-- !Our Team Area 
     <div class="our-team-area sction-padding-100-70">
         <div class="container">
@@ -448,10 +304,6 @@
             </div>
         </div> 
     </div> 
-=======
-    <!--  !Our Team Area -->
-
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
     <!--  Our Team Area End -->
 
 
@@ -462,38 +314,7 @@
 
 
     <!-- JS here -->
-<<<<<<< HEAD
-    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
-    <script src="js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/isotope.pkgd.min.js"></script>
-    <script src="js/ajax-form.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/imagesloaded.pkgd.min.js"></script>
-    <script src="js/scrollIt.js"></script>
-    <script src="js/jquery.scrollUp.min.js"></script>
-    <script src="js/wow.min.js"></script>
-    <script src="js/nice-select.min.js"></script>
-    <script src="js/jquery.slicknav.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/plugins.js"></script>
-
-    <!-- Contact js-->
-    <script src="js/contact.js"></script>
-    <script src="js/jquery.ajaxchimp.min.js"></script>
-    <script src="js/jquery.form.js"></script>
-    <script src="js/jquery.validate.min.js"></script>
-    <script src="js/mail-script.js"></script>
-
-    <!-- Main js-->
-    <script src="js/main.js"></script>
-    <script src="./js/script.js"></script> <!-- custom js -->
-=======
     <?php include 'componants/scriptTags.php'; ?>
->>>>>>> 95f4483cb45d79ceb5594b502a81965eaf718c65
 
 
     </body>
